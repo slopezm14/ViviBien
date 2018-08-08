5075b69ad3c5f536bd3d9d89d9245380b6fe5837
Humberto Lopez Menendez <slopezm14@miumg.edu.gt>
2018-08-07 20:43:47 -0600

    Rutas para accesar a controladores

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,21 @@
 Route::get('/','ViewsController@index');
 
 
-
+/* Resource Controllers */
+Route::resource('bitacora','BitacoraController');
+Route::resource('categodili','CateDiliController');
+Route::resource('departamento','DepartamentoController');
+Route::resource('desarrollador','DesarrolladorController');
+Route::resource('destinosub','DestinoSubController');
+Route::resource('genero','GeneroController');
+Route::resource('municipio','MunicipioController');
+Route::resource('proyecto','ProyectoController');
+Route::resource('relacionfam','RelacionFamController');
+Route::resource('requisito','RequisitoController');
+Route::resource('rol','RolController');
+Route::resource('telefono','TelefonosController');
+Route::resource('tipoaccion','TipoAccionController');
+Route::resource('tipoingreso','TipoIngresoController');
+Route::resource('tipotelefono','TipoTelefController');
+Route::resource('unidadtrabajo','UniTrabajoController');
+Route::resource('usuario','UsuarioController');
