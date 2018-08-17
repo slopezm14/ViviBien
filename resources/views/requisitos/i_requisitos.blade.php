c681dbd5d25219076b620e94508b45652ae3676e
Kervins Pineda <kpinedac2@miumg.edu.gt>
2018-08-16 19:37:38 -0700

    Sesiones

--- resources/views/requisitos/i_requisitos.blade.php
+++ resources/views/requisitos/i_requisitos.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.principal')
-
+@if(Session::has('message'))
+<div class="alert alert-info alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  {{Session::get('message')}}
+</div>
+@endif
 @section('content')
 
 <div class="row">
